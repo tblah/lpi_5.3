c201739fc28ddbc15669a6a970fde2deeace5c78
Tom Eccles <t@freedommail.info>
2014-07-01 17:31:31 +0100

    Finished.
    I changed my mind to a cleaner implementation so many of the helper functions were removed

--- atomic.c
+++ atomic.c
@@ -12,6 +12,8 @@
 // atomic_append filename num-bytes [x]
 // appends num-bytes to filename (creating it if nessecary). If x is specified then O_APPEND should be replaced with a call to lseek before every write.
 
+typedef enum {false, true} bool;
+
 void errExit(char* errMsg)
 {
 	int errorno = errno;
@@ -22,25 +24,6 @@ void errExit(char* errMsg)
 	fflush(stdout);
 
 	exit(EXIT_FAILURE);
-}
-
-int fileOpen(char filename[], int extraFlags) {	// DRY. Opens, checks for erros then returns the fd
-	int fd = open(filename, extraFlags | O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH); // rw-rw-r--
-	if (fd == -1)
-		errExit("open");
-	
-	return fd;
-}
-
-void fileWrite(int fd) { // DRY, writes one byte of dummy data
-	char dummy = 'a';
-	if (write(fd, &dummy, 1) == -1)
-		errExit("write");
-}
-
-void fileClose(int fd) { // DRY
-	if (close(fd) == -1)
-		errExit("close");
 }
 
 long int getLong(char string[]) { // gets a long from a string
@@ -54,20 +37,50 @@ long int getLong(char string[]) { // gets a long from a string
 	if (errno != 0)			// check to see if strtol experienced an error
 		errExit("strtol");
 
-	if (firstCharNotNumber != '\0')	// if the string contained invalid charichters
-		errExit("Expecting a number in base 10, got a string");
+	if (*firstCharNotNumber != '\0')	// if the string contained invalid charichters
+		errExit("strtol: Expected base 10 number, got something else");
 
 	return result;
+}
+
+void writeAppend(char filename[], long int numBytes, bool atomic) {	// this function does all of the work
+	int openFlags = O_CREAT | O_WRONLY;
+
+	if (atomic == true)
+		openFlags |= O_APPEND;
+
+	int fd = open(filename, openFlags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH); // rw-rw-r--
+	if (fd == -1)
+		errExit("open");
+
+	// write numBytes bytes to the file
+	char dummy = 'a';	// dummy data to be written to the file
+
+	for (int i = 0; i < numBytes; i++) {
+		// if we are allowing the race condition then we lseek before we write
+		if (atomic == false) {
+			if (lseek(fd, 0, SEEK_END) == -1)
+				errExit("lseek");
+		}
+
+		// write one byte of dummy data to the file
+		if (write(fd, &dummy, 1) == -1)
+			errExit("write");
+	}
+
+	if (close(fd) == -1)
+		errExit("close");
+
 }
 
 int main(int argc, char* argv[])
 {
 	// deal with command line arguements
 	if (argc == 4 && strcmp(argv[3], "x") == 0) {
-		// lseek & write
+		writeAppend(argv[1], getLong(argv[2]), false);
 	}
 	else if (argc == 3) {
-		// write with append
+		writeAppend(argv[1], getLong(argv[2]), false);
 	}
 	else {
 		printf("USAGE: %s filename num-bytes [x]\n", argv[0]);
